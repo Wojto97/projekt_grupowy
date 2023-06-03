80a01240d47b729ef86f75369ad7a8bf1874cd9b
Wojciech Gródecki <wojtekgrodecki97@gmail.com>
2023-06-03 10:26:28 +0200

    initialization project

---
+++ src/test/java/com/example/projekt_grupowy/ProjektGrupowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt_grupowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektGrupowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
