13abe9d315ea5bf421a92aa4b7752a620d60105a
Mateusz Myalski <mateusz.myalski@gmail.com>
2023-06-17 10:46:08 +0200

    Working create_table base

--- src/main/resources/sql/create_table.sql
+++ src/main/resources/sql/create_table.sql
@@ -1,18 +1,18 @@
-drop table Kraj if exists;
+drop table if exists Kraj;
 create table Kraj (
   id_kraj INT NOT NULL AUTO_INCREMENT,
   nazwa VARCHAR(255),
   PRIMARY KEY (id_kraj)
-)
+);
 
-drop table Rola if exists;
+drop table if exists Rola;
 create table Rola (
   id_rola INT NOT NULL AUTO_INCREMENT,
   nazwa VARCHAR(255),
   PRIMARY KEY (id_rola)
-)
+);
 
-drop table Osoba if exists;
+drop table if exists Osoba;
 create table Osoba (
   id_osoba INT NOT NULL AUTO_INCREMENT,
   rola_id INT,
@@ -28,9 +28,9 @@ create table Osoba (
   aktywny BIT,
   ilosc_nieudanych_prob_logowania INT,
   PRIMARY KEY (id_osoba)
-)
+);
 
-drop table Przedmiot if exists;
+drop table if exists Przedmiot;
 create table Przedmiot (
   id_przedmiot INT NOT NULL AUTO_INCREMENT,
   id_osoba INT,
@@ -40,18 +40,18 @@ create table Przedmiot (
   termin_najblizszych_zajec DATETIME,
   termin_egzaminu DATETIME,
   PRIMARY KEY (id_przedmiot)
-)
+);
 
-drop table Ocena if exists;
+drop table if exists Ocena;
 create table Ocena (
   id_osoba INT NOT NULL AUTO_INCREMENT,
   id_przedmiotu INT,
   wartosc DECIMAL(10,2),
   data_wpisania_oceny DATETIME,
   PRIMARY KEY (id_osoba)
-)
+);
 
-drop table PlanZajec if exists;
+drop table if exists PlanZajec;
 create table PlanZajec (
   id_plan_zajec INT NOT NULL AUTO_INCREMENT,
   id_sala_wykladowa INT,
@@ -59,42 +59,41 @@ create table PlanZajec (
   godzina_rozpoczęcia VARCHAR(255),
   godzina_zakonczenia VARCHAR(255),
   PRIMARY KEY (id_plan_zajec)
+);
 
-)
-
-drop table SalaWykladowa if exists;
+drop table if exists SalaWykladowa;
 create table SalaWykladowa (
   id_sala_wykladowa INT NOT NULL AUTO_INCREMENT,
   numer_sali VARCHAR(5),
   adres_sali VARCHAR(255),
   PRIMARY KEY (id_sala_wykladowa)
-)
+);
 
-drop table Biblioteka if exists;
-create table Biblioteka {
+drop table if exists Biblioteka;
+create table Biblioteka (
   id_biblioteka INT NOT NULL AUTO_INCREMENT,
   id_ksiazka VARCHAR(255),
   data_wypozyczenia DATETIME,
   data_rezerwacji DATETIME,
   PRIMARY KEY (id_biblioteka)
-}
+);
 
-drop table Ksiazka if exists;
-create table Ksiazka {
+drop table if exists Ksiazka;
+create table Ksiazka (
   id_ksiazka INT NOT NULL AUTO_INCREMENT,
   tytuł VARCHAR(255),
   id_wypozyczył INT,
   id_zarezerwował INT,
   po_terminie_oddania BIT,
   PRIMARY KEY (id_ksiazka)
-}
+);
 
-drop table Platnosc if exists;
-create table Platnosc {
+drop table if exists Platnosc;
+create table Platnosc (
   id_platnosc INT NOT NULL AUTO_INCREMENT,
   data DATETIME,
   wartosc DECIMAL(10,2),
   zaplanowana BIT,
   PRIMARY KEY (id_platnosc)
-}
+);
 
