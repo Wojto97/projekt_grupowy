c110a59ff1c80605ff6db5b1a5825bc4d93b983b
Mateusz Myalski <mateusz.myalski@gmail.com>
2023-06-17 10:42:18 +0200

    Fixed typos, types, etc in sql

--- src/main/resources/sql/create_table.sql
+++ src/main/resources/sql/create_table.sql
@@ -1,100 +1,100 @@
 drop table Kraj if exists;
 create table Kraj (
-  id_kraj INTEGER NOT NULL AUTO_INCREMENT,
-  nazwa VARCHAR(100),
-  PRIMARY key (id_kraj)
+  id_kraj INT NOT NULL AUTO_INCREMENT,
+  nazwa VARCHAR(255),
+  PRIMARY KEY (id_kraj)
 )
 
 drop table Rola if exists;
 create table Rola (
-  id_rola INTEGER NOT NULL AUTO_INCREMENT,
-  nazwa VARCHAR(100),
-  PRIMARY key (id_rola)
+  id_rola INT NOT NULL AUTO_INCREMENT,
+  nazwa VARCHAR(255),
+  PRIMARY KEY (id_rola)
 )
 
 drop table Osoba if exists;
 create table Osoba (
-  id_osoba INTEGER NOT NULL AUTO_INCREMENT,
-  rola_id VARCHAR(100),
-  kraj_id VARCHAR(100),
-  imie VARCHAR(100),
-  drugie_imie VARCHAR(100),
-  nazwisko VARCHAR(100),
-  pesel VARCHAR(100),
-  ulica VARCHAR(100),
-  miejscowosc VARCHAR(100),
-  email VARCHAR(100),
-  haslo VARCHAR(100),
-  aktywny Boolean,
-  ilosc_nieudanych_prob_logowania INTEGER,
-  PRIMARY key (id_osoba)
+  id_osoba INT NOT NULL AUTO_INCREMENT,
+  rola_id INT,
+  kraj_id INT,
+  imie VARCHAR(255),
+  drugie_imie VARCHAR(255),
+  nazwisko VARCHAR(255),
+  pesel VARCHAR(255),
+  ulica VARCHAR(255),
+  miejscowosc VARCHAR(255),
+  email VARCHAR(255),
+  haslo VARCHAR(255),
+  aktywny BIT,
+  ilosc_nieudanych_prob_logowania INT,
+  PRIMARY KEY (id_osoba)
 )
 
 drop table Przedmiot if exists;
 create table Przedmiot (
-  id_przedmiot INTEGER NOT NULL AUTO_INCREMENT,
-  id_osoba VARCHAR(100),
-  id_sala_wykladowa VARCHAR(100),
-  nazwa VARCHAR(100),
-  opis_zajec VARCHAR(100),
+  id_przedmiot INT NOT NULL AUTO_INCREMENT,
+  id_osoba INT,
+  id_sala_wykladowa INT,
+  nazwa VARCHAR(255),
+  opis_zajec VARCHAR(255),
   termin_najblizszych_zajec DATETIME,
   termin_egzaminu DATETIME,
-  PRIMARY key (id_przedmiot)
+  PRIMARY KEY (id_przedmiot)
 )
 
 drop table Ocena if exists;
 create table Ocena (
-  id_osoba INTEGER NOT NULL AUTO_INCREMENT,
-  id_przedmiotu VARCHAR(100),
-  wartosc double,
+  id_osoba INT NOT NULL AUTO_INCREMENT,
+  id_przedmiotu INT,
+  wartosc DECIMAL(10,2),
   data_wpisania_oceny DATETIME,
-  PRIMARY key (id_osoba)
+  PRIMARY KEY (id_osoba)
 )
 
-drop table PlanZajęc if exists;
-create table PlanZajęc (
-  id_plan_zajec INTEGER NOT NULL AUTO_INCREMENT,
-  id_sala_wykladowa VARCHAR(100),
-  id_przedmiotu VARCHAR(100),
-  godzina_rozpoczęcia VARCHAR(100),
-  godzina_zakończenia VARCHAR(100),
-  PRIMARY key (id_plan_zajec)
+drop table PlanZajec if exists;
+create table PlanZajec (
+  id_plan_zajec INT NOT NULL AUTO_INCREMENT,
+  id_sala_wykladowa INT,
+  id_przedmiotu INT,
+  godzina_rozpoczęcia VARCHAR(255),
+  godzina_zakonczenia VARCHAR(255),
+  PRIMARY KEY (id_plan_zajec)
 
 )
 
 drop table SalaWykladowa if exists;
 create table SalaWykladowa (
-  id_sala_wykladowa INTEGER NOT NULL AUTO_INCREMENT,
-  numer_sali VARCHAR(100),
-  adres_sali VARCHAR(100),
-  PRIMARY key (id_sala_wykladowa)
+  id_sala_wykladowa INT NOT NULL AUTO_INCREMENT,
+  numer_sali VARCHAR(5),
+  adres_sali VARCHAR(255),
+  PRIMARY KEY (id_sala_wykladowa)
 )
 
 drop table Biblioteka if exists;
 create table Biblioteka {
-  id_biblioteka INTEGER NOT NULL AUTO_INCREMENT,
-  id_ksiazka VARCHAR(100),
+  id_biblioteka INT NOT NULL AUTO_INCREMENT,
+  id_ksiazka VARCHAR(255),
   data_wypozyczenia DATETIME,
-  DATETIME_rezerwacji DATETIME,
-  PRIMARY key (id_biblioteka)
+  data_rezerwacji DATETIME,
+  PRIMARY KEY (id_biblioteka)
 }
 
 drop table Ksiazka if exists;
 create table Ksiazka {
-  id_ksiazka INTEGER NOT NULL AUTO_INCREMENT,
-  tytuł VARCHAR(100),
-  wypozyczył Osoba,
-  zarezerwował Osoba,
-  po_terminie_oddania boolean,
-  PRIMARY key (id_ksiazka)
+  id_ksiazka INT NOT NULL AUTO_INCREMENT,
+  tytuł VARCHAR(255),
+  id_wypozyczył INT,
+  id_zarezerwował INT,
+  po_terminie_oddania BIT,
+  PRIMARY KEY (id_ksiazka)
 }
 
-drop table Płatnosc if exists;
-create table Płatnosc {
-  id_platnosc INTEGER NOT NULL AUTO_INCREMENT,
+drop table Platnosc if exists;
+create table Platnosc {
+  id_platnosc INT NOT NULL AUTO_INCREMENT,
   data DATETIME,
-  wartosc double ,
-  zaplanowana boolean ,
-  PRIMARY key (id_platnosc)
+  wartosc DECIMAL(10,2),
+  zaplanowana BIT,
+  PRIMARY KEY (id_platnosc)
 }
 
