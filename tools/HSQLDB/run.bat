1431882ef5153747d9f8a8bb8fffccb1fcc6cfc6
Piotr Bartela <walletkie@gmail.com>
2023-06-26 21:43:25 +0200

    final fixes

--- tools/HSQLDB/run.bat
+++ tools/HSQLDB/run.bat
@@ -2,4 +2,4 @@
 java -classpath lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:hsqldb/hemrajdb --dbname.0 testdb
 
 # /hsqldb/data
-java -classpath ../lib/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing
+java -classpath lib/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing
